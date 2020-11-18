a87f5cdc43c7cb0650aa15973978635d997cd3a3
EdwinVargas22 <edwin.vargas9662@gmail.com>
2020-11-17 19:47:21 -0800

    Added comments

--- 10-Sqlalchemy-Challenge/Climate App/app.py
+++ 10-Sqlalchemy-Challenge/Climate App/app.py
@@ -1,3 +1,4 @@
+# Import Dependencies
 import numpy as np
 import datetime as dt
 import sqlalchemy
@@ -36,8 +37,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/station<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start-YYYY-MM-DD<br/>"
+        f"/api/v1.0/start-YYYY-MM-DD/end"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -73,13 +74,16 @@ def precipitation():
 @app.route("/api/v1.0/station")
 def station():
 
+
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    # Perform a query to get all of the data from the station table
     station_list = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
 
     session.close()
 
-    # Create a dictionary for the station and append to a list 
+    # Create a dictionary from the station_list data and append to a list of station_data
     station_data = []
     for station, name, latitude, longitude, elevation in station_list:
         station_dict = {}
@@ -95,12 +99,18 @@ def station():
 @app.route("/api/v1.0/tobs")
 def tobs():
 
+    # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    # Get the last year
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Perform a query to find the most active station
     most_active_station = session.query(Measurement.station, func.count(Measurement.station).label("count")).\
             group_by(Measurement.station).\
             order_by(desc('count')).all()
 
+    # Perform a query to get all of the data from the most active station
     most_active_station_info = session.query(Measurement.station, Measurement.date, Measurement.prcp, Measurement.tobs).\
             filter(Measurement.station=='USC00519281').\
             filter(Measurement.date > last_year).\
@@ -108,6 +118,7 @@ def tobs():
 
     session.close()
 
+    # Create a dictionary from the most_active_station_info data and append to a list of most_active_station_data
     most_active_station_data = []
     for station, date, prcp, tobs in most_active_station_info:
         tobs_dict = {}
@@ -123,11 +134,16 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 def start(start):
 
+    # Create our session (link) from Python to the DB
+    session=Session(engine)
+
+    # Perform a query to get all of the data from the measurement table but using the input start date as the filter
     start_date_data = session.query(Measurement.station, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
         filter(Measurement.date >= start).\
         group_by(Measurement.station).\
         order_by(Measurement.date).all()
 
+    session.close()
     
     return jsonify(start_date_data)
 
@@ -135,10 +151,18 @@ def start(start):
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
 
+    # Create our session (link) from Python to the DB
+    session=Session(engine)
+
+    # Perform a query to get all of the data from the measurement table but using the input start date and input end date as the filter
     start_end_data = session.query(Measurement.station, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
         filter(Measurement.date >= start, Measurement.date < end).\
         group_by(Measurement.station).\
         order_by(Measurement.date).all()
+
+    session.close()
+
+    return jsonify(start_end_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
