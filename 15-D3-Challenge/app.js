56abc2dc7a5e676ff13ae8251d91e4de5f22037c
EdwinVargas22 <edwin.vargas9662@gmail.com>
2021-01-08 23:04:22 -0800

    created scale and linear

--- 15-D3-Challenge/app.js
+++ 15-D3-Challenge/app.js
@@ -1,1 +1,39 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("data.csv").then(function(usCensus) {
+
+    usCensus.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(usCensus, d => d.poverty)])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(usCensus, d => d.healthcare)])
+        .range([height, 0]);
+
+    
+})
