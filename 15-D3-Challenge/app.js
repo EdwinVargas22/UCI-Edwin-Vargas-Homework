4c433ef8092cfb97b09a4e1c5f9c217d37e45d6b
EdwinVargas22 <edwin.vargas9662@gmail.com>
2021-01-09 22:58:42 -0800

    Created the axis, circle, and append it

--- 15-D3-Challenge/app.js
+++ 15-D3-Challenge/app.js
@@ -28,12 +28,63 @@ d3.csv("data.csv").then(function(usCensus) {
     });
 
     var xLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(usCensus, d => d.poverty)])
+        .domain([d3.min(usCensus, d => d.poverty), d3.max(usCensus, d => d.poverty)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(usCensus, d => d.healthcare)])
+        .domain([d3.min(usCensus, d => d.healthcare), d3.max(usCensus, d => d.healthcare)])
         .range([height, 0]);
 
-    
-})
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(usCensus)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "12")
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
+
+    var circleLabels = chartGroup.selectAll(null).data(usCensus).enter().append("text");
+
+    circleLabels
+        .attr("x", function(d) {
+            return xLinearScale(d.poverty);
+        })
+        .attr("y", function(d) {
+            return yLinearScale(d.healthcare);
+        })
+        .text(function(d) {
+            return d.abbr;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .attr("fill", "white");
+
+
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
+}).catch(function(error) {
+    console.log(error);
+});
