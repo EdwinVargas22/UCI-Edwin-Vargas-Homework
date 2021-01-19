e7b321358aff452cf0d46275587185e512b92703
EdwinVargas22 <edwin.vargas9662@gmail.com>
2021-01-18 21:02:49 -0800

    created a new for loop

--- 16-Leaflet-Challenge/Leaflet-Step-1/static/js/logic.js
+++ 16-Leaflet-Challenge/Leaflet-Step-1/static/js/logic.js
@@ -5,7 +5,7 @@ var myMap = L.map("mapid", {
 });
 
 // Earthquakes GeoJSON URL 
-var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
   
 // Create the tile layer that will be the background of our map
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -18,53 +18,94 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
 }).addTo(myMap);
 
 // Set Up Legend
-var legend = L.control({ 
-    position: "bottomright" 
-});
+// var legend = L.control({ 
+//     position: "bottomright" 
+// });
 
 // Add the layer control, insert a div with the class of "legend"
-legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "legend"),
-    return div;
-};
+// legend.onAdd = function() {
+//     var div = L.DomUtil.create("div", "legend"),
+//     return div;
+// };
 
-// Add the legend to the map
-legend.addTo(myMap);
+// // Add the legend to the map
+// legend.addTo(myMap);
 
   // Perform an API call to the all earthquakes data
 d3.json(earthquakesURL, function(earthquakeData) {
-    var quakeProperty  = earthquakeData.features.properties;
-    var quakeGeo  = earthquakeData.features.geometry;
+    var quakeAll  = earthquakeData.features;
+    // var quakeGeo  = earthquakeData.features.geometry;
+    var quakeGeo2 = earthquakeData.features.geometry.coordinates[2];
+
+    for (var i = 0; i < quakeGeo2.length; i++) {
+
+        var color = "";
+        if (quakeGeo2[i] < 11) {
+            color = "green";
+        }
+        else if (quakeGeo2[i] >= 10 && quakeGeo2[i] <= 30) {
+            color = "light green";
+        }
+        else if (quakeGeo2[i] >= 30 && quakeGeo2[i] <= 50) {
+            color = "light orange";
+        }
+        else if (quakeGeo2[i] >= 50 && quakeGeo2[i] <= 70) {
+            color = "orange";
+        }
+        else if (quakeGeo2[i] >= 70 && quakeGeo2[i] <= 90) {
+            color = "dark orange";
+        }
+        else {
+            color = "red";
+        }
+
+
+    }
 
     // Loop through the earthquake data array
-    for (var i = 0; i < quakeGeo.length; i++) {
+    // for (var i = 0; i < quakeGeo.length; i++) {
 
-    var color = "";
-    if (quakeGeo.coordinates[2] < 11) {
-        color = "green";
-    }
-    else if (quakeGeo.coordinates[2] >= 10 & quakeGeo.coordinates[2] <= 30) {
-        color = "light green";
-    }
-    else if (quakeGeo.coordinates[2] >= 30 & quakeGeo.coordinates[2] <= 50) {
-        color = "light orange";
-    }
-    else if (quakeGeo.coordinates[2] >= 50 & quakeGeo.coordinates[2] <= 70) {
-        color = "orange";
-    }
-    else if (quakeGeo.coordinates[2] >= 70 & quakeGeo.coordinates[2] <= 90) {
-        color = "dark orange";
-    }
-    else {
-        color = "red";
-    }
+    //     var color = "";
+    //     if (quakeGeo.coordinates[2] < 11) {
+    //         color = "green";
+    //     }
+    //     else if (quakeGeo.coordinates[2] >= 10 && quakeGeo.coordinates[2] <= 30) {
+    //         color = "light green";
+    //     }
+    //     else if (quakeGeo.coordinates[2] >= 30 && quakeGeo.coordinates[2] <= 50) {
+    //         color = "light orange";
+    //     }
+    //     else if (quakeGeo.coordinates[2] >= 50 && quakeGeo.coordinates[2] <= 70) {
+    //         color = "orange";
+    //     }
+    //     else if (quakeGeo.coordinates[2] >= 70 && quakeGeo.coordinates[2] <= 90) {
+    //         color = "dark orange";
+    //     }
+    //     else {
+    //         color = "red";
+    //     }
 
-    L.circle((time?), quakeGeo.coordinates[1, 0], {
+    L.circle(quakeAll.geometry.coordinates[1, 0], {
         fillOpacity: 0.75,
         color: "white",
         fillColor: color,
-    //     radius: quakeProperty.mag * 1000
-    // }).bindPopup(quakeProperty.title + "<br> Type: " + quakeProperty.type + "<br> Place: " + quakeProperty.place + "<br> Time: " + quakeProperty.time.format("h:mm:ss A") + "<br> Magnitude: " + quakeProperty.mag);
+        radius: quakeAll.properties.mag * 1000
+    }).bindPopup(quakeAll.properties.title + "<br> Type: " + quakeAll.properties.type + "<br> Place: " + quakeAll.properties.place 
+    + "<br> Time: " + quakeAll.properties.time.format ("h:mm:ss A") or New Date (quakeAll.properties.time) + "<br> Magnitude: " + quakeAll.properties.mag);
     }
+
+
+
+    // Set Up Legend
+    var legendInfo = L.control({ 
+        position: "bottomright" 
+    });
+
+    // Add the layer control, insert a div with the class of "legend"
+    legendInfo.onAdd = function() {
+        var div = L.DomUtil.create("div", "legend");
+        return div;
+    };
+
   
 });
